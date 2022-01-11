3239aa37af9d7b1a23c961c01785454096452d4a
Areen-Rath <70901420+Areen-Rath@users.noreply.github.com>
2022-01-11 22:03:11 +0530

    Add files via upload

---
+++ Coding-Folder-Maker.py
@@ -0,0 +1,7 @@
+import os
+
+class_no = input("Class No.: ")
+
+os.mkdir("C:/Users/Monali/Desktop/VS Code/" + class_no)
+os.mkdir("C:/Users/Monali/Desktop/VS Code/" + class_no + "/" + class_no)
+os.mkdir("C:/Users/Monali/Desktop/VS Code/" + class_no + "/Project")
